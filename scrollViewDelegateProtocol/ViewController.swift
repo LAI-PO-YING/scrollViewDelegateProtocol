a13bcaac2b7ffcf49278c64631ec66075c43af45
Ivan Lai <229705jack@gmail.com>
2021-07-09 15:43:09 +0800

    first commit

--- scrollViewDelegateProtocol/ViewController.swift
+++ scrollViewDelegateProtocol/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageDot: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func pageDotChange(_ sender: UIPageControl) {
+        let point = CGPoint(x: scrollView.bounds.width * CGFloat(sender.currentPage), y: 0)
+        scrollView.setContentOffset(point, animated: true)
+    }
+    
 
+}
 
+extension ViewController: UIScrollViewDelegate{
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.bounds.width
+        pageDot.currentPage = Int(page)
+    }
 }
 
